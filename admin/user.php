2f45b5040639a13462f1ec48db11d64b622b422d
@joshua40 <josh@knockmedia.com>
2018-11-15 10:16:12 -0500

    admin construction started

---
+++ admin/user.php
@@ -0,0 +1,20 @@
+<?php
+//======================================================================
+// ADMIN USER
+//======================================================================
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php include_once (realpath(dirname(__FILE__, 2).'/include/head.php')); ?>
+  </head>
+  <body>
+    <?php include_once (realpath(dirname(__FILE__, 2).'/include/header.php')); ?>
+    <main role="main" class="container">
+      <!-- Page Content Goes Here -->
+      <h1> Welcome Admin </h1>
+    </main>
+    <?php include_once (realpath(dirname(__FILE__, 2).'/include/footer.php')); ?>
+  </body>
+</html>
