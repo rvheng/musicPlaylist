f5b6aac30e55e9e6e39b6f81e0c0569b4714dd1e
@joshua40 <josh@knockmedia.com>
2018-11-25 17:28:09 -0500

    update authenticate file to send user and admin to their directories

--- db/authenticate.php
+++ db/authenticate.php
@@ -14,13 +14,14 @@ if (isset($_POST['submit'])) {
     $error = "Username or Password is empty!";
     return $error;
   }
-}else{
+} else {
   /* Check the Username and Password */
   if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $salt = 'hashbrowns';
     $username = $_POST['username'];
     //$pass = $_POST["password"]; // Fix use salt
     $pass = crypt($_POST["password"], $salt );
+    $user_roll = 0;
     
     // Protect against MYSQL injection
     $username = stripslashes($username);
@@ -30,27 +31,43 @@ if (isset($_POST['submit'])) {
 
     // SQL query to fetch information and find match user
     // should this use prepare?
-    $query = mysqli_query($db_connection, "SELECT * FROM user WHERE password='" . $pass . "' AND username='" . $username . "'"); 
-    $rows = mysqli_num_rows($query);
+    // $query = mysqli_query($db_connection, "SELECT * FROM user WHERE password='" . $pass . "' AND username='" . $username . "'"); 
+    // $rows = mysqli_num_rows($query);
+    $select_user = $db_connection->prepare(
+      "SELECT username,role_id FROM user WHERE username = ? AND password = ? LIMIT 1");
+    $select_user->bind_param("ss", $username, $pass);
+    $select_user->execute();
+    $select_user->bind_result($user_name, $user_role);
+    $select_user->store_result();
 
-    if($rows == 1) {
-      session_start(); 
-      # This area needs to send users and admins to there own directory
-      $_SESSION['login_user']=$username;
-      header("location: ./../user");
+    if($select_user->num_rows == 1) {
+      if($select_user->fetch()) {
+
+        session_start(); 
+        # This area needs to send users and admins to there own directory
+        $_SESSION['login_user']=$username;
+        $_SESSION['user_role'] = $user_role;
+
+        if ($_SESSION['user_role'] == 1) {
+          header("location: ./../user");
+        } elseif ($_SESSION['user_role'] == 2) {
+          header("location: ./../admin/");
+        } else {
+          //header("location: ../");
+          echo "problem...";
+        }
+      
+      }
     }else{
       echo "Username or Password did not match!";
-      //$error = "Username or Password did not match!";
-      //$_SESSION['error']=$error;
-      //header("location: ./../index.php");
+      //header("location: ./../");
     }
     // close the mysql connection
-    mysqli_close($db_connection); 
+    $select_user->close();
   }else{
 
     // remove all session variables
     session_unset(); 
-
     // destroy the session 
     session_destroy(); 
   }
