5b832e36c38b3bb3227c3e084cbc3e761ba8a377
@joshua40 <josh@knockmedia.com>
2018-12-02 17:10:48 -0500

    additions to search playlist

--- admin/playlist_search.php
+++ admin/playlist_search.php
@@ -16,8 +16,46 @@ include_once (realpath(dirname(__FILE__, 2).'/db/session.php'));
     <main role="main" class="container">
       <div class="row justify-content-sm-center">
         <div class="col-sm-9">
-<!-- Page Content Goes Here -->
-          <h1> Welcome <?php echo $user_name; ?> </h1>
+          
+          <div class="row">
+            <div class="col-md-6">
+              <h1> Playlist Search </h1>
+            </div>
+            <div class="col-md-6 text-right">
+              <form method="post" action="<?php echo BASE_URL; ?>/admin/playlist_search.php">
+                <input type="hidden" name="userid_edit" value="<?php echo $row["user_id"]; ?>">
+                <button type="submit" class="btn btn-success">Create Playlist <i class="fas fa-plus"></i></button>
+              </form>
+            </div>
+          </div>
+          <!-- ENTER SEARCH HERE -->
+          <form action="<?php echo BASE_URL; ?>/admin/playlist_search.php" method="post">
+            <div class="form-row">
+              <small id="helpBlock" class="form-text text-muted">
+                Helpful hint...
+              </small>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-sm-10">
+                <input type="text" class="form-control" id="fname" placeholder="Playlist Name" name="playlist">
+              </div>
+              <div class="form-group col-sm-2">
+                <button type="submit" class="btn btn-primary">Search</button>
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-md-12">
+              <?php
+              /* Error Message */
+              if (isset($error)) {
+                // uses bootstrap alert style for error messages
+                echo '<div class="alert alert-danger" role="alert">' . $error . '</div>';
+              }
+            ?>
+            </div>
+            </div>
+          </form>
+
         </div>
       </div>
     </main>
